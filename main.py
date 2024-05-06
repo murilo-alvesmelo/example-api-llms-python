d94311cd1bf062063d259ee39d0c46fc07558880
Murilo Alves Melo <murilo.melo72@gmail.com>
2024-05-05 22:08:04 -0300

    add new router

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 
 from fastapi import FastAPI
+
 from app_sentiment_analysis.routes.api import router as analise_sentimental_router
+from app_translate.routes.api import router as translate_router
+
 app = FastAPI()
 
 app.include_router(analise_sentimental_router, prefix="/analise_sentimental_router")
+app.include_router(translate_router, prefix="/translate")
